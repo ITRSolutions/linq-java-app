7c544170662d7c2b67ec1a90c2cf9184a4e1006b
abdulrazzakjakati <30927556+abdulrazzakjakati@users.noreply.github.com>
2025-05-01 22:40:09 +0530

    UI bug fixing - Inprocess

--- src/main/resources/static/admin_panel/js/common.js
+++ src/main/resources/static/admin_panel/js/common.js
@@ -184,7 +184,15 @@ $(document).ready(function() {
     });
 });
 
-    function formatDate(dateString) {
-        var date = new Date(dateString);
-        return date.toLocaleString();
-    }
+function formatDate(dateString) {
+    var date = new Date(dateString);
+    return date.toLocaleString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+}
